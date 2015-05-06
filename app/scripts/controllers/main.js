0e315df7b374117cd6e00fedaebf06e783a7ea31
Justin <farzad_cpu83@yahoo.com>
2015-05-06 16:44:57 -0700

    Feature: Analytics on website traffic over time
    we show a line chart that shows the total number of visits to the website per week for a given period of time. The user specifies a period of time using the bootstrap datepicker.
    we show the number of times events have happened each week between the start dates and end dates.
    
    For example, if the start date is 2/1/15 and the end date  is 2/17/15, the chart contains the number of times events have occurred from:
    2/1/15 - 2/6/15 (week1)
    2/6/15 - 2/12/15 (week 2)
    2/13/15 - 2/17/15 (week 3 - incomplete week)

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,10 +11,16 @@ angular.module('zyringApp')
     .controller('MainCtrl', ['$scope', 'VisitDataResource',
         function ($scope, VisitDataResource) {
 
-            VisitDataResource.query().$promise.then(function (result) {
-                $scope.currentData = result;
+
+
+            var currentYear = new Date().getFullYear();
+            $scope.startDate = new Date(currentYear,0,1);
+            $scope.endDate = new Date().toDateString();
+
+            function calculateData() {
                 calculateCityData();
-            });
+                calculateWeeklyCounts();
+            }
 
             function calculateCityData() {
                 var cityData = _.countBy($scope.currentData, function (elem) {
@@ -25,6 +31,83 @@ angular.module('zyringApp')
                 $scope.cityLabels = _.keys(cityData);
                 $scope.cityData = _.values(cityData);
             }
+
+            var weeklEpoch = 604800;
+            function calculateWeeklyCounts() {
+
+                $scope.weeklySeries = ['Visits'];
+                var startEpoch = new Date($scope.startDate).getTime() / 1000;
+                var endEpoch = new Date($scope.endDate).getTime() / 1000;
+                var numOfWeek = Math.ceil((endEpoch - startEpoch) / weeklEpoch);
+
+                $scope.weeklyLabels = [];
+                for(var i = 1 ; i <= numOfWeek; i++) {
+                    $scope.weeklyLabels.push('Week ' + i);
+                }
+
+                var weeklyCounts = [];
+                for (var k = 0 ; k < numOfWeek; k++) {
+                    weeklyCounts.push(0);
+                }
+
+                _.each($scope.currentData, function(data) {
+                    var timeStamp = data.timestamp;
+                    //if the timestamp falls between the start and end date
+                    if (timeStamp >= startEpoch && timeStamp <= endEpoch) {
+                        //calculate the week
+                        var weekNo = Math.floor((timeStamp - startEpoch) / weeklEpoch);
+                        weeklyCounts[weekNo] += 1;
+                    }
+                });
+                $scope.weeklyCounts = [weeklyCounts];
+            }
+
+
+            VisitDataResource.query().$promise.then(function (result) {
+                $scope.allData = result;
+                $scope.currentData = $scope.allData;
+                calculateData();
+
+            });
+
+
+            $scope.dateOptions = {
+                formatYear: 'yy',
+                startingDay: 1
+            };
+
+            $scope.openStart = function ($event) {
+                $event.preventDefault();
+                $event.stopPropagation();
+                $scope.openedStart = true;
+            };
+
+            $scope.openEnd = function ($event) {
+                $event.preventDefault();
+                $event.stopPropagation();
+                $scope.openedEnd = true;
+            };
+
+            $scope.$watch('endDate', function (newEnd) {
+
+                if ($scope.allData) {
+                    var newEndTimestamp = new Date(newEnd).getTime() / 1000;
+                    $scope.currentData = _.filter($scope.allData, function (elem) {
+                        return elem.timestamp <= newEndTimestamp;
+                    });
+                    calculateData();
+                }
+
+            });
+            $scope.$watch('startDate', function (newStart) {
+                if ($scope.allData) {
+                    var newStartTimeStamp = new Date(newStart).getTime() / 1000;
+                    $scope.currentData = _.filter($scope.allData, function (elem) {
+                        return elem.timestamp >= newStartTimeStamp;
+                    });
+                    calculateData();
+                }
+            });
 
         }
     ]
